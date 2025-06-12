909f6d2ed6ed8b370ecbd54fad2cad07ea29f670
Sahil Patil <sahilpatil32267@gmail.com>
2025-06-12 14:59:59 +0530

    Signup logic with password encryption is done

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,56 @@
+const UserService = require("../services/user-service");
+
+const userService = new UserService();
+
+const create = async (req, res) => {
+  try {
+    const response = await userService.create({
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    return res.status(201).json({
+      success: true,
+      data: response,
+      message: "Successfully created a new user",
+      err: {},
+    });
+  } catch (error) {
+    console.log("Something went wrong in the controller layer");
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to create a new user",
+      err: error,
+    });
+  }
+};
+
+const destroy = async (req, res) => {
+  try {
+    const response = await userService.destroy({
+      userId: req.params.userId,
+      password: req.body.password,
+    });
+
+    return res.status(200).json({
+      data: response,
+      success: true,
+      message: "Successfully deleted a user",
+      err: {},
+    });
+  } catch (error) {
+    console.log("Something went wrong in the controller layer");
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to delete a user",
+      err: error,
+    });
+  }
+};
+
+module.exports = {
+  create,
+  destroy,
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index");
 
 const app = express();
 
 const prepareAndStartServer = () => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
+
   app.listen(PORT, () => {
     console.log(`Server is Started on PORT: ${PORT}`);
   });
