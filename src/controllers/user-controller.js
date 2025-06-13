c361ae20f078fd7efa174c8f5332da77dfa950a4
Sahil Patil <sahilpatil32267@gmail.com>
2025-06-13 21:36:10 +0530

    Added user isAuth function

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -62,7 +62,7 @@ const signIn = async (req, res) => {
       data: response,
       message: "Successfully signed in",
       err: {},
-    }); 
+    });
   } catch (error) {
     console.log("Something went wrong in the controller layer");
     return res.status(500).json({
@@ -74,8 +74,30 @@ const signIn = async (req, res) => {
   }
 };
 
+const isAuthenticated = async (req, res) => {
+  try {
+    const token = req.headers["x-access-token"];
+    const response = await userService.isAuthenticated(token);
+    return res.status(200).json({
+      success: true,
+      data: response,
+      err: {},
+      message: "User is authenticated and token is valid",
+    });
+  } catch (error) {
+    console.log("Something went wrong in the controller layer");
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to verify authentication",
+      err: error,
+    });
+  }
+};
+
 module.exports = {
   create,
   destroy,
   signIn,
+  isAuthenticated,
 };
